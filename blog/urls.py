1e5252b56543aa03f77cd37cdb53a7e2352ed40a
Esila2 <eslamalale@gmail.com>
2025-05-02 19:29:47 +0330

    blog app created

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet
+
+app_name = 'blog'
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+
+urlpatterns = router.urls
